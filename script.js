1ab054a30c55c6100e41ebe35a7d241bcb5d737a
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-08 11:04:59 -0700

    constructor object to create tiles for board

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+//General
+const body = document.body;
 // Array
 const mark = [0, 0, 0,
               0, 0, 0,
               0, 0, 0]
 
 //Obj
-function Tile(mark){
-    this.mark = mark;
+const Tile = function (mark){
+    this.mark=mark;
 }
+
+//Board
+const board = document.createElement('div'); 
+board.className='board';
+body.append(board);
+
+//Individual Tile
+function createTiles(){
+    for(i=0;i<9;i++){
+        const div = document.createElement('div');
+        div.className = `div ${i}`;
+        const tile = new Tile(mark[i]);
+        div.append(tile.mark);
+        board.append(div);
+    }
+}
+
+createTiles();
