bdda004ecf1ed458becf411a63a22b0c6117b018
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-15 14:31:08 -0700

    checkTie function not working when there is a tie and I want to disable the board

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const body = document.body;
 const gameBoard = document.createElement('div');
 const gamePlayers=document.createElement('div');
+const gameTurn=document.createElement('div');
 const gameResult=document.createElement('div');
 
 gameBoard.className='game';
@@ -10,8 +11,15 @@ body.append(gameBoard);
 gamePlayers.className='players';
 body.append(gamePlayers);
 
+gameTurn.className='turn';
+body.append(gameTurn);
+
 gameResult.className='results';
 body.append(gameResult);
+
+const btnReset=document.createElement('button');
+btnReset.textContent='Clear'
+body.append(btnReset)
 
 //Value
 let tiles = [];
@@ -29,6 +37,7 @@ Tile.prototype.info=function(){
 function makeTile(){
     for(i=1;i<=9;i++){
         const btn = document.createElement('button');
+        btn.className='tile'
         gameBoard.append(btn);
 
         const tile = {
@@ -48,7 +57,7 @@ function makeTile(){
 
 makeTile();
 
-const btns = document.querySelectorAll('button')
+const btns = document.querySelectorAll('.tile')
 
 
 // Player
@@ -66,43 +75,95 @@ const playerTwo = new Player('player 2', 'X');
 console.log(playerOne.info());
 console.log(playerTwo.info());
 
-const players = [
-    playerOne,
-    playerTwo
-]
+const players = []
+players.push(playerOne);
+players.push(playerTwo);
+
+
+gamePlayers.textContent=`${playerOne.info()}\n${playerTwo.info()}`
 
 // Function for Switching player Turns
+let activePlayer = players[0];
+
 function switchPlayerTurn(){
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    if(activePlayer===playerOne){
-        btns.forEach((btn)=>{
-            btn.addEventListener('click', ()=>{
-                btn.textContent='O'
-            })
-        })
-    }
-    else if(activePlayer===playerTwo){
-        btns.forEach((btn)=>{
-            btn.addEventListener('click', ()=>{
-                btn.textContent='X'
-            })
-        })
-    }
 };
 
-function playGame(){
-    //activate switchPlayerTurn()
-    //announce winner when three in a row
+const getActivePlayer = () => activePlayer;
+
+btns.forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        if(btn.textContent===''){
+        btn.textContent= activePlayer===players[0]?'O':'X'
+        switchPlayerTurn();
+        announceResult();
+        checkTie();
+        }
+    })
+})
+
+switchPlayerTurn();
+
+function clearGame(){   
+    //reset
+    btns.forEach((btn)=>{
+        btn.textContent='';
+        btn.disabled=false;
+    })
+    
 }
 
-function threeInARow(){
-    // arrangements of different array announces win
-    // first row
-    // second row
-    // third row
-    // first column
-    // second column
-    // third column
-    // first diagonal
-    // second diagonal
+btnReset.addEventListener('click',()=>{
+    clearGame();
+})
+
+const btn1 = document.getElementById('1');
+const btn2 = document.getElementById('2');
+const btn3 = document.getElementById('3');
+const btn4 = document.getElementById('4');
+const btn5 = document.getElementById('5');
+const btn6 = document.getElementById('6');
+const btn7 = document.getElementById('7');
+const btn8 = document.getElementById('8');
+const btn9 = document.getElementById('9');
+
+const btnTiles = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9];
+
+const winningCombo=[
+    [btn1,btn2,btn3],
+    [btn4,btn5,btn6],
+    [btn7,btn8,btn9],
+    [btn1,btn4,btn7],
+    [btn2,btn5,btn8],
+    [btn3,btn6,btn9],
+    [btn1,btn5,btn9],
+    [btn3,btn5,btn7]
+]
+
+function announceResult(){
+    for(let i=0;i<winningCombo.length;i++){
+        const combo=winningCombo[i];
+
+        if(
+            combo[0].textContent!==''&&
+            combo[0].textContent===combo[1].textContent&&
+            combo[0].textContent===combo[2].textContent
+        ){
+            gameResult.textContent=`${activePlayer.name} wins!`
+
+        btns.forEach(btn=>btn.disabled=true);
+        return true;
+        }
+    }
+    return false;
+}
+
+function checkTie(){
+    const allFilled= btns.every(btn=>btn.textContent!=='');
+    if(allFilled && !announceResult()){
+        gameResult.textContent='No Contest. Play again?'
+        btns.forEach(btn=>btn.disabled=true);
+        return true;
+    }
+return false;
 }
